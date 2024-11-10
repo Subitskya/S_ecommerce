9277dd2c183d8d6ec7092a16e9dae2b33080a74e
Anup Shakya <dev2krizmatic.com>
2024-11-10 16:32:28 +0545

    Add missing script for showing image in rider create form

--- resources/views/admin/riders/create.blade.php
+++ resources/views/admin/riders/create.blade.php
@@ -36,7 +36,10 @@
                         <div class="form-group col-md-6">
                             <label for="profile_pic">Profile Pic</label>
                             <input type="file" class="form-control" id="profile_pic" name="profile_pic"
-                                value="{{old('profile_pic')}}" required>
+                                value="{{old('profile_pic')}}" onchange="getImagePreview(event,'profile_images_view')"
+                                required>
+                            <div id="profile_images_view">
+                            </div>
                         </div>
                         <div class="form-group col-md-6">
                             <label for="dob">DOB</label>
@@ -51,16 +54,23 @@
                             <label for="license_no">License No</label>
                             <input type="text" class="form-control" id="license_no" name="license_no"
                                 value="{{old('license_no')}}" required>
+
                         </div>
                         <div class="form-group col-md-6">
                             <label for="license_front">License Front Image</label>
                             <input type="file" class="form-control" id="license_front" name="license_front"
-                                value="{{old('license_front')}}" required>
+                                value="{{old('license_front')}}"
+                                onchange="getImagePreview(event,'license_front_images_view')" required>
+                            <div id="license_front_images_view">
+                            </div>
                         </div>
                         <div class="form-group col-md-6">
                             <label for="national_id">Nationa ID</label>
                             <input type="file" class="form-control" id="national_id" name="national_id"
-                                value="{{old('national_id')}}" required>
+                                value="{{old('national_id')}}"
+                                onchange="getImagePreview(event,'national_id_images_view')" required>
+                            <div id="national_id_images_view">
+                            </div>
                         </div>
                     </div>
                     <button type="submit" class="btn btn-primary">Create Rider</button>
@@ -69,4 +79,16 @@
         </div>
     </div>
 </div>
+<script>
+    function getImagePreview(event,divId){
+        var image = URL.createObjectURL(event.target.files[0]);
+        var imageDiv = document.getElementById(divId);
+        var imagetag = document.createElement('img');
+        
+        imagetag.src = image;
+        imagetag.width = "150";
+        imagetag.style.padding = "5px";
+        imageDiv.appendChild(imagetag);
+    }
+</script>
 @stop
